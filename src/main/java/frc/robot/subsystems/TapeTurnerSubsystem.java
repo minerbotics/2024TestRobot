647dc1febf3212abc10ba356707dada6f4b5a556
FRC Team 4827 - Minerbotics <60262671+minerbotics@users.noreply.github.com>
2023-10-18 19:56:41 -0400

    Start playing around with PID on tape turner

--- src/main/java/frc/robot/subsystems/TapeTurnerSubsystem.java
+++ src/main/java/frc/robot/subsystems/TapeTurnerSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAlternateEncoder.Type;
 
@@ -17,11 +19,40 @@ public class TapeTurnerSubsystem extends SubsystemBase {
 
   private final CANSparkMax m_motor;
   private final RelativeEncoder m_encoder;
+  private final SparkMaxPIDController m_pidController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   /** Creates a new ExampleSubsystem. */
   public TapeTurnerSubsystem() {
     m_motor = new CANSparkMax(Constants.OperatorConstants.kTapeTurnerMotor, MotorType.kBrushless);
+    m_pidController = m_motor.getPIDController();
     m_encoder = m_motor.getAlternateEncoder(Type.kQuadrature, 4096);
+    // PID coefficients
+    kP = 0.1; 
+    kI = 1e-4;
+    kD = 1; 
+    kIz = 0; 
+    kFF = 0; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
   }
 
   public void turn(boolean clockwise) {
@@ -31,7 +62,12 @@ public class TapeTurnerSubsystem extends SubsystemBase {
     } else {
       m_motor.set(speed);
     }
-    SmartDashboard.putNumber("TapeTurner", m_encoder.getPosition());
+    SmartDashboard.putNumber("TapeTurner Position", m_encoder.getPosition());
+    SmartDashboard.putNumber("TapeTurner Velocity", m_encoder.getVelocity());
+  }
+
+  public void turnToPosition(double position) {
+    m_pidController.setReference(position, ControlType.kPosition);
   }
 
   public void stop() {
